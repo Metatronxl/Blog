7e57836973dbb917ed61000c38f78e28106757ba
Metatronxl <8b:b6:7d:29:b0:c3:da:1d:d3:3d:58:6f:d7:76:32:9e>
2017-08-09 15:04:12 +0800

    添加kindeditor富文本编辑器
    自定义config文件，要注意的K.create(‘textarea=[name=body]’),其中body为post的字段

---
+++ static/blog/assets/js/kindeditor-4.1.10/config.js
@@ -0,0 +1,9 @@
+/**
+ * Created by xulei2 on 2017/8/9.
+ */
+KindEditor.ready(function (K) {
+    window.editor = K.create('textarea[name=body]', {
+        width: '80',
+        height: '20',
+    });
+});
